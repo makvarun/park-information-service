1a3a8cab8bc88184ee5a97bb481cedc51d030c5f
makvarun <126195300+makvarun@users.noreply.github.com>
2024-03-22 19:00:14 -0700

    initial project commit

---
+++ src/main/java/com/nationalpark/parkinformation/ParkInformationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nationalpark.parkinformation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkInformationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkInformationServiceApplication.class, args);
+	}
+
+}
